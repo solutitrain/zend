9814cd888a00c82ee29fb0c17e93218985be7855
MARCILEI MENDES DE FREITAS <marcilei.freitas@solutinet.com.br>
2019-06-01 11:13:46 -0300

    Agora vai

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -21,4 +21,8 @@ class IndexController extends AbstractActionController
     {
         return new ViewModel();
     }
+    public function galeriaAction()
+    {
+        return new ViewModel();
+    }
 }
